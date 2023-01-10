f2a6344955ed1edac8dfd9f9b83055eecf14c0cd
Kristian <kristianzlatkovv@gmail.com>
2023-01-10 23:39:31 +0200

    Added Token model

---
+++ app/Models/Token.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Token extends Model
+{
+    use HasFactory;
+
+    protected $table = 'personal_access_tokens';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'tokenable_type',
+        'tokenable_id',
+        'name',
+        'token',
+        'abilities',
+        'last_used_at',
+    ];
+}
