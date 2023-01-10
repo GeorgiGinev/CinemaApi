638035405c44616072fd768a5913a0f0c90c3c24
Kristian <kristianzlatkovv@gmail.com>
2023-01-10 23:37:24 +0200

    Cinema route name change

--- routes/v1/cinema.php
+++ routes/v1/cinema.php
@@ -3,13 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Cinema\Http\Controllers\CinemaController;
 
-Route::controller(CinemaController::class)->middleware('auth:sanctum')->prefix('cinema')->group(function() {
+Route::controller(CinemaController::class)->prefix('cinemas')->group(function() {
     Route::get('/{id}', 'getOne');
+    Route::get('', 'getMany');
     Route::post('', 'create');
     Route::put('/{id}', 'update');
     Route::delete('/{id}', 'delete');
-});
-
-Route::controller(CinemaController::class)->middleware('auth:sanctum')->prefix('cinemas')->group(function() {
-    Route::get('', 'getMany');
 });
